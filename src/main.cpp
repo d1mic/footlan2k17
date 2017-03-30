f6a6f0a9ce0033959d36010a146ed24541c6be6f
ivke080 <ivke11080@gmail.com>
2017-03-30 15:25:05 +0200

    Iscrtan prozor

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 
 int main(int argc, char *argv[])
 {	
 	
-	std::cout << "Footlan2k17" << std::endl;
-	
+	sf::RenderWindow window(sf::VideoMode(600, 800), "Footlan2k17");
+	sf::CircleShape circle(50);
+	circle.setFillColor(sf::Color::Red);
+	circle.setOutlineColor(sf::Color::Blue);
+	circle.setOutlineThickness(10);
+	circle.setPosition(100, 100);
+
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed) {
+				window.close();
+			}
+		}
+
+		window.clear(sf::Color::Black);
+		window.draw(circle);
+		window.display();
+	}
+	//	g++ main.cpp -lsfml-window -lsfml-system -lsfml-graphics
+
 	return 0;
 }
